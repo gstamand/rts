84fbaad7802898b04d7f684110a88af3f394b502
Garrett St. Amand <garrettstamand@yahoo.com>
2020-10-30 22:02:39 -0600

    movement, camera, and selection controls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float panSpeed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 pos = transform.position;
+
+    }
+
+    public void MoveCameraForward()
+    {
+        Vector3 pos = transform.position;
+        pos.z += panSpeed * Time.deltaTime;
+        transform.position = pos;
+    }
+    public void MoveCameraBackward()
+    {
+        Vector3 pos = transform.position;
+        pos.z -= panSpeed * Time.deltaTime;
+        transform.position = pos;
+    }
+    public void MoveCameraLeft()
+    {
+        Vector3 pos = transform.position;
+        pos.x -= panSpeed * Time.deltaTime;
+        transform.position = pos;
+    }
+    public void MoveCameraRight()
+    {
+        Vector3 pos = transform.position;
+        pos.x += panSpeed * Time.deltaTime;
+        transform.position = pos;
+    }
+
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class InputController : MonoBehaviour
+{
+
+
+    public SelectionController selectionController;
+    public CameraController cameraController;
+
+    public float panBorderThickness = 10f;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            selectionController.MoveSelectedUnits();
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            selectionController.StartSelectionBox();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            selectionController.ReleaseSelectionBox();
+        }
+        if (Input.GetMouseButton(0))
+        {
+            selectionController.UpdateSelectionBox(Input.mousePosition);
+        }
+        if(Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            cameraController.MoveCameraForward();
+        }
+        if (Input.mousePosition.y <= panBorderThickness)
+        {
+            cameraController.MoveCameraBackward();
+        }
+        if (Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            cameraController.MoveCameraRight();
+        }
+        if (Input.mousePosition.x <= panBorderThickness)
+        {
+            cameraController.MoveCameraLeft();
+        }
+    }
+
+
+    
+
+}
